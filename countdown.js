3570f34a41fe8edd716881b4598a15cc17fa1cf2
medilies <medilies@gmail.com>
2022-05-13 22:36:31 +0100

    FEAT: timer class

--- countdown.js
+++ countdown.js
@@ -1,59 +1,74 @@
 const countDown = () => {
     document.querySelectorAll("[autoCountdown]").forEach((el) => {
-        const TARGET_TIMESTAMP = targetTimestamp(el.dataset.countdownDuration);
+        const timer = new Timer(el.dataset.countdownDuration);
 
         const TIMER_FORMAT = el.dataset.countdownFormat;
 
-        const COUNTDOWN_STEP_SECONDS = (function () {
-            const COUNTDOWN_STEP = new Timespan(el.dataset.countdownStep);
-            return COUNTDOWN_STEP.getTotalSeconds();
-        })();
-
-        let remainingMilliSeconds = TARGET_TIMESTAMP - Date.now();
+        const countdownStep = new Timespan(el.dataset.countdownStep);
 
         // init
-        el.innerHTML = moment.utc(remainingMilliSeconds).format(TIMER_FORMAT);
+        el.innerHTML = moment
+            .utc(timer.getRemainingSeconds()) // TODO custom formatter
+            .format(TIMER_FORMAT);
 
         setInterval(() => {
-            remainingMilliSeconds = TARGET_TIMESTAMP - Date.now();
-
-            if (remainingMilliSeconds <= 0) {
+            if (timer.getRemainingSeconds() <= 0) {
                 window[el.dataset.countdownHandler]();
                 return;
             }
 
-            remainingMilliSeconds =
-                remainingMilliSeconds < 0 ? 0 : remainingMilliSeconds;
-
             el.innerHTML = moment
-                .utc(remainingMilliSeconds)
+                .utc(timer.getRemainingSeconds())
                 .format(TIMER_FORMAT);
-        }, COUNTDOWN_STEP_SECONDS * 1000);
+        }, countdownStep.getTotalMilliSeconds());
     });
 };
 
 /**
  *
- * @param {number} remainingSeconds
- * @returns
  */
-function targetTimestamp(remainingSeconds) {
-    return Date.now() + parseInt(remainingSeconds) * 1000;
+class Timer {
+    /**
+     *
+     * @param {number} initDuration in seconds
+     */
+    constructor(initDuration) {
+        this.initDuration = initDuration;
+
+        this._setTargetTimestamp();
+    }
+
+    /**
+     *
+     * @param {number} remainingSeconds
+     */
+    _setTargetTimestamp() {
+        this.targetTimestamp = Date.now() + parseInt(this.initDuration) * 1000;
+    }
+
+    getRemainingSeconds() {
+        // console.log(this.targetTimestamp - Date.now());
+        return this.targetTimestamp - Date.now();
+    }
 }
 
 /**
  *
  */
 class Timespan {
+    /**
+     *
+     * @param {string} timespanStr [<two-digits>h][<two-digits>m][<two-digits>s]
+     */
     constructor(timespanStr) {
-        this.timespanStr = timespanStr;
+        this.timespanStr = timespanStr.toLocaleLowerCase();
 
-        this.setParsedTimespanComposers();
-        this.setTotalSeconds();
-        this.consolelog();
+        this._setParsedTimespanComposers();
+        // this._consolelog();
+        this._setTotalSeconds();
     }
 
-    setParsedTimespanComposers() {
+    _setParsedTimespanComposers() {
         const HMS = this.timespanStr.match(
             /(?:(?<h>\d{0,2})h)?(?:(?<m>\d{0,2})m)?(?:(?<s>\d{0,2})s)?/
         );
@@ -63,7 +78,7 @@ class Timespan {
         this.seconds = parseInt(HMS.groups.s) || 0;
     }
 
-    setTotalSeconds() {
+    _setTotalSeconds() {
         this.totalSeconds =
             this.hours * 3600 + this.minutes * 60 + this.seconds;
     }
@@ -72,7 +87,11 @@ class Timespan {
         return this.totalSeconds;
     }
 
-    consolelog() {
+    getTotalMilliSeconds() {
+        return this.totalSeconds * 1000;
+    }
+
+    _consolelog() {
         console.log({
             hours: this.hours,
             minutes: this.minutes,
