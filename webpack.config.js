08393e20d54ef55c2bafacf2a4ed1b908afadd2b
medilies <medilies@gmail.com>
2022-05-14 23:05:51 +0100

    Trying to publish the package

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 module.exports = {
     mode: "production",
+    target: "web",
     entry: "./src/index.js",
     output: {
         filename: "countdouwn.min.js",
         path: path.resolve(__dirname, "dist"),
+        globalObject: "this",
     },
 };
